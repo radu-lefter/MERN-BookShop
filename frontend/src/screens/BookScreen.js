cf4571a72793ae08b1c970640d7919d8cc9cdeec
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-10-26 14:53:38 +0100

    implemented redux for books

--- frontend/src/screens/BookScreen.js
+++ frontend/src/screens/BookScreen.js
@@ -1,84 +1,90 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import {useParams} from  'react-router-dom'
-import axios from 'axios'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import { useParams } from 'react-router-dom';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listBookDetails } from '../actions/bookActions';
 
 const BookScreen = () => {
-
   const params = useParams();
-  const [book, setBook] = useState({})
+  const dispatch = useDispatch();
+
+  const bookDetails = useSelector((state) => state.bookDetails);
+  const { loading, error, book } = bookDetails;
 
   useEffect(() => {
-    const fetchBook = async () => {
-      const { data } = await axios.get(`/api/books/${params.id}`)
-
-      setBook(data)
-    }
-
-    fetchBook()
-  }, [params.id])
+    dispatch(listBookDetails(params.id));
+  }, [dispatch, params.id]);
 
   return (
     <>
-      <Link className='btn btn-light my-3' to='/'>
+      <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={book.image} alt={book.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{book.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={book.rating}
-                text={`${book.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${book.price}</ListGroup.Item>
-            <ListGroup.Item>Description: {book.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant='flush'>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={book.image} alt={book.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${book.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{book.name}</h3>
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    {book.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                  </Col>
-                </Row>
+                <Rating
+                  value={book.rating}
+                  text={`${book.numReviews} reviews`}
+                />
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  className='btn-block'
-                  type='button'
-                  disabled={book.countInStock === 0}
-                >
-                  Add To Cart
-                </Button>
-              </ListGroup.Item>
+              <ListGroup.Item>Price: ${book.price}</ListGroup.Item>
+              <ListGroup.Item>Description: {book.description}</ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </>
-  )
-}
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${book.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
 
-export default BookScreen
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {book.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={book.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default BookScreen;

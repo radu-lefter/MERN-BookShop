088516609e4c984909704db0f83d058482eaf8f1
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-10-26 17:05:45 +0100

    created user controller and routes

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler'
+import Book from '../models/bookModel.js'
+
+// @desc    Fetch all books
+// @route   GET /api/books
+// @access  Public
+const getBooks = asyncHandler(async (req, res) => {
+  const Books = await Book.find({})
+
+  res.json(Books)
+})
+
+// @desc    Fetch single book
+// @route   GET /api/Books/:id
+// @access  Public
+const getBookById = asyncHandler(async (req, res) => {
+  const book = await Book.findById(req.params.id)
+
+  if (book) {
+    res.json(book)
+  } else {
+    res.status(404)
+    throw new Error('Book not found')
+  }
+})
+
+export { getBooks, getBookById }
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,34 +1,11 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Book from '../models/bookModel.js'
+import {
+  getBooks,
+  getBookById,
+} from '../controllers/bookController.js'
 
-// @desc    Fetch all books
-// @route   GET /api/books
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const books = await Book.find({})
-    res.json(books)
-  })
-)
-
-// @desc    Fetch single book
-// @route   GET /api/books/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const book = await Book.findById(req.params.id)
-
-    if (book) {
-      res.json(book)
-    } else {
-      res.status(404)
-      throw new Error('Book not found')
-    }
-  })
-)
+router.route('/').get(getBooks)
+router.route('/:id').get(getBookById)
 
 export default router
